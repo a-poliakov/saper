5f19f9a33f684ba793c367ddf356a7119351de43
avpgenium <avpgenium@yandex.ru>
2016-04-07 00:00:09 +0500

    добавлен тест для проверки создания поля

--- src/main/java/Field.java
+++ src/main/java/Field.java
@@ -3,5 +3,14 @@ public class Field {
 
     public Field() {
         gride = new Cell[10][10];
+        for(int i = 0; i < gride.length; i++){
+            for(int j = 0; j < gride[i].length; j++){
+                gride[i][j] = Cell.unknown;
+            }
+        }
+    }
+
+    public Cell[][] getGride() {
+        return gride;
     }
 }
--- src/test/java/TestFild.java
+++ src/test/java/TestFild.java
@@ -1,10 +1,36 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
-public class TestFild {
-    @Test
-    public void testConstructor(){
-        Assert.assertNotNull(new Field());
+public class TestFild{
+    Field field;
+
+    @Before
+    public void init(){
+       // testConstructor();
+        field = new Field();
     }
 
+    @Test
+    public void testConstructor(){
+        Cell[][] grid = field.getGride();
+        Assert.assertNotNull("grid is null", grid);
+        for (Cell[] cellRow : grid) {
+            for (Cell cell : cellRow) {
+                Assert.assertEquals(Cell.unknown, cell);
+            }
+        }
+        Assert.assertNotNull("testConstructor failed", new Field());
+    }
+
+    @Test
+    public void testCreateGrid(){
+        Cell[][] grid = field.getGride();
+        Assert.assertNotNull("grid is null", grid);
+        for (Cell[] cellRow : grid) {
+            for (Cell cell : cellRow) {
+                Assert.assertEquals(Cell.mine, cell);
+            }
+        }
+    }
 }
